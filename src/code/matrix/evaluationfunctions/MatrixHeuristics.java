8a6e6b4080c98266fa2500244dbb40a2ffff2ab2
samar fathallah <samar.fathallah@student.guc.edu.eg>
2021-11-24 22:29:19 +0200

    Modified h1 and added h2

--- src/code/matrix/evaluationfunctions/MatrixHeuristics.java
+++ src/code/matrix/evaluationfunctions/MatrixHeuristics.java
@@ -1,7 +1,11 @@
 package code.matrix.evaluationfunctions;
 
+import java.util.ArrayList;
+
 import code.matrix.general.MatrixState;
+import code.matrix.helpers.Position;
 import code.matrix.objects.Hostage;
+import code.matrix.objects.Pad;
 import code.searchproblem.general.SearchTreeNode;
 
 public class MatrixHeuristics {
@@ -9,28 +13,50 @@ public class MatrixHeuristics {
     public static int h1(SearchTreeNode node) {
         MatrixState state = new MatrixState(node.state);
         Hostage nearestHostage = null;
-        int minDistance = Integer.MAX_VALUE;
+        int nearestHostageDistance = Integer.MAX_VALUE;
         for (Hostage hostage: state.hostages) {
             if ((!hostage.isAgent && !hostage.position.equals(state.telephoneBooth.position) && !hostage.isCarried) || (hostage.isAgent && !hostage.isKilled)) {
-                int distance = hostage.position.getManhattanDistance(state.neo.position);
-                if (distance < minDistance) {
+                int distance = getMinimumDistance(state.neo.position, hostage.position, state.pads);
+                if (distance < nearestHostageDistance) {
                     nearestHostage = hostage;
-                    minDistance = distance;
+                    nearestHostageDistance = distance;
                 }
             }
         }
+       
         if (nearestHostage == null) {
             if (state.neo.position.equals(state.telephoneBooth.position)) {
                 return state.neo.carriedHostages.size() == 0 ? 0 : 1;
             }
-            return state.telephoneBooth.position.getManhattanDistance(state.neo.position);
+            return getMinimumDistance(state.neo.position,state.telephoneBooth.position, state.pads);
         }
-        int boothDistance = state.telephoneBooth.position.getManhattanDistance(nearestHostage.position);
-        return 1 + minDistance + boothDistance;
+        
+        int hostageDistance=getMinimumDistance(state.neo.position,nearestHostage.position, state.pads);
+        int boothDistance=getMinimumDistance(nearestHostage.position,state.telephoneBooth.position, state.pads);
+        return hostageDistance + boothDistance;
     }
 
     public static int h2(SearchTreeNode node) {
-        return 1;
+        MatrixState state = new MatrixState(node.state);
+        int remainingHostages = 0;
+        for (Hostage hostage: state.hostages) {
+            if ((!hostage.isAgent && !hostage.position.equals(state.telephoneBooth.position) && !hostage.isCarried) || (hostage.isAgent && !hostage.isKilled)) {
+                remainingHostages++;
+            }
+        }
+        return remainingHostages;
+    }
+
+    public static int getMinimumDistance(Position start,Position end, ArrayList<Pad> pads){
+        int minDistanceWithoutPad=start.getManhattanDistance(end);
+        int minDistanceWithPad=Integer.MAX_VALUE;
+        for(Pad pad:pads){
+            int DistanceWithPad=start.getManhattanDistance(pad.start)+end.getManhattanDistance(pad.finish)+1;
+            if(DistanceWithPad<minDistanceWithPad){
+                minDistanceWithPad=DistanceWithPad;
+            }
+        }
+        return Integer.min(minDistanceWithPad, minDistanceWithoutPad);
     }
     
 }
