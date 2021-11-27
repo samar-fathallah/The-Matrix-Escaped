84170335c78e8b711ff62c7856659501f4bf2b1b
Mohammad Marwan Sultan <mohammad.marwan.sultan@gmail.com>
2021-11-27 19:28:58 +0200

    Returned test cases to last posted version

--- src/tests/App.java
+++ src/tests/App.java
@@ -5,8 +5,8 @@ import code.Matrix;
 public class App {
 
     public static void main(String[] args) {
-        String initialGrid = "5,5;2;0,4;1,4;0,1,1,1,2,1,3,1,3,3,3,4;1,0,2,4;0,3,4,3,4,3,0,3;0,0,30,3,0,80,4,4,80";
-        Matrix.solve(initialGrid, "UC", true);
+        String grid = "5,5;2;0,4;1,4;0,1,1,1,2,1,3,1,3,3,3,4;1,0,2,4;0,3,4,3,4,3,0,3;0,0,30,3,0,80,4,4,80";
+        Matrix.solve(grid, "AS1", true);
     }
     
 }
